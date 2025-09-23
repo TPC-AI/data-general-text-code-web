0f263c18754ab4d8c22ec9fa9471d1311ac00982
Yadu Babuji <yadudoc1729@gmail.com>
2025-09-22 19:16:20 -0500

    Add `skip_insertion` kwarg to all LSHBloom based workflows and components
    * When `skip_insertion` is enabled, unique entries found are not inserted into the index. This enables deduplicating against an index without modifying it.

--- deduplication/args.py
+++ deduplication/args.py
@@ -99,5 +99,10 @@ def parse_args():
 		help="If set, will skip the minhashing step of each workflow (useful if minhashes have been precomputed at minhash_dir)",
 		action="store_true"
 	)
+	parser.add_argument(
+		"--skip-insertion",
+		help="If set, will skip inserting unique documents into the index (works only with LSHBloom)",
+		action="store_true"
+	)
 
 	return parser.parse_args()
